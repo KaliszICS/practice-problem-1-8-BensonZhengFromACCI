4aef804c43dca93f582f31364883b9c5c17c187a
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2026-04-14 09:11:18 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 class PracticeProblem {
 
-	
+	static Scanner s = new Scanner(System.in);
   	public static void main(String[] args) {
 		
 		q1();
@@ -21,7 +21,6 @@ class PracticeProblem {
   	public static void q1() {
 		// From input, recieve two integers from the user and add them together.  Output the result.
 		
-		Scanner s = new Scanner(System.in);
 		System.out.print("Input a number: ");
 		int num1 = s.nextInt();
 		System.out.print("Input a number: ");
@@ -31,7 +30,6 @@ class PracticeProblem {
 
 	public static void q2() {
 		// From input recieve two integers.  Output the quotient rounded down.
-		Scanner s = new Scanner(System.in);
 		System.out.print("Input a number: ");
 		int num1 = s.nextInt();
 		System.out.print("Input a number: ");
@@ -41,14 +39,12 @@ class PracticeProblem {
 
 	public static void q3() {
 		// Output the phrase "hello "Mr. Kalisz" have you seen my work yet?"
-		Scanner s = new Scanner(System.in);
 		System.out.println("hello \"Mr. Kalisz\" have you seen my work yet?");
 	}
 
 	public static void q4() {
 		// From input recieve two numbers (can be decimal fractions).  
 		// Output the first number plus two, then multiplied by the second number.
-		Scanner s = new Scanner(System.in);
 		System.out.print("Input a number: ");
 		double num1 = s.nextDouble();
 		System.out.print("Input a number: ");
